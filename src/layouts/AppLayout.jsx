fe734b6df48fdfafa184256cf874fc715ef2a50c
Himmel-al <andhika24ti@mahasiswa.pcr.ac.id>
2026-06-15 09:27:44 +0700

    daisyui

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
+import Navbar from "./Navbar";
 
 export default function AppLayout() {
   return (
     <div style={{ background: "#0d0f14", minHeight: "100vh", display: "flex" }}>
       <div style={{ display: "flex", flexDirection: "row", flex: 1 }}>
-        <Sidebar />
+        <Sidebar/>
+        {/* <Navbar/> */}
         <div style={{ flex: 1, display: "flex", flexDirection: "column", background: "#0d0f14", overflow: "hidden" }}>
           <Header />
           <div style={{ flex: 1, overflowY: "auto", background: "#0d0f14" }}>
